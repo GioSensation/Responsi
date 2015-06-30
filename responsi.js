ec0eba0e53edc047d2c5e99f151ebb41efe4b32e
Emanuele <feliziani.emanuele@gmail.com>
2015-06-30 15:24:17 +0200

    Important update read the details
    This is the first important update. It adds the possibility to fire responsi after the initial init by calling `responsi.fire()` and allows to use the various breakpoints set for responsi, so that you can do something like `if ( responsi.viewport > responsi.phones )`. So cool.
    
    Signed-off-by: Emanuele <feliziani.emanuele@gmail.com>

--- responsi.js
+++ responsi.js
@@ -10,47 +10,53 @@ var responsi = {
 	// We set viewport and dpi at the object level so they can be easily accessed outside responsi. It's just a helper. I know I will use them. Note: I couldn't find the browser support for devicePixelRatio, so I just throw a fallback in case that fails.
 	viewport : document.documentElement.clientWidth,
 	dpi : window.devicePixelRatio || 1,
-	// Init fires the whole thing.
-	init : function( responsiConfig ) {
-		var viewport = this.viewport,
-			dpi = this.dpi,
-			responsiSize = 'large',
-			defaultConfig = {
+	config : function ( responsiConfig ) {
+		// Configure viewport sizes
+		var defaultConfig = {
 				phones	: 480,
 				tabs	: 768,
 				laps	: 1200,
 				desks	: 1900
 			};
-		
 		responsiConfig = typeof responsiConfig !== 'undefined' ? responsiConfig : defaultConfig;
-		
+		this.phones = responsiConfig.phones;
+		this.tabs = responsiConfig.tabs;
+		this.laps = responsiConfig.laps;
+		this.desks = responsiConfig.desks;
+	},
+	fire : function() {
+		// This is the actual thing. It can be fired when adding responsi elements dynamically.
+		var viewport = this.viewport,
+			dpi = this.dpi,
+			responsiSize = 'large';
+
 		/***************** SMALL *****************/
 		if (
-			viewport <= responsiConfig.phones /* 480 */ // all smartphones, regardless of dpi
+			viewport <= this.phones /* 480 */ // all smartphones, regardless of dpi
 			||
-			viewport < responsiConfig.tabs /* 768 */ && dpi < 1.5 // up to regular dpi phablets and small tablets (no iPad)
+			viewport < this.tabs /* 768 */ && dpi < 1.5 // up to regular dpi phablets and small tablets (no iPad)
 			) {
 				responsiSize = 'small';
 			}
-		
+
 		/***************** MEDIUM *****************/
 		if (
-			viewport > responsiConfig.phones /* 480 */ && viewport < responsiConfig.laps /* 1200 */ && dpi > 1.5 // hidpi phablets, tablets and hidpi small computers (dunno whether they even exist)
+			viewport > this.phones /* 480 */ && viewport < this.laps /* 1200 */ && dpi > 1.5 // hidpi phablets, tablets and hidpi small computers (dunno whether they even exist)
 			||
-			viewport >= responsiConfig.tabs /* 768 */ && viewport < responsiConfig.desks /* 1900 */ && dpi < 1.5 // regular tablets, laptops & desktop
+			viewport >= this.tabs /* 768 */ && viewport < this.desks /* 1900 */ && dpi < 1.5 // regular tablets, laptops & desktop
 			) {
 				responsiSize = 'medium';
 			}
-		
+
 		/***************** LARGE *****************/
 		if (
-			viewport >= responsiConfig.laps /* 1200 */ && dpi > 1.5 // hidpi laptops
+			viewport >= this.laps /* 1200 */ && dpi > 1.5 // hidpi laptops
 			||
-			viewport >= responsiConfig.desks /* 1900 */ // big desktops
+			viewport >= this.desks /* 1900 */ // big desktops
 			) {
 				responsiSize = 'large';
 			}
-		
+
 		/***************** RSPONSI MAGIC HAPPENS HERE *****************/
 		// Get the source URI.
 		function getResponsiSrc(el) {
@@ -61,7 +67,7 @@ var responsi = {
 						 el.getAttribute('data-responsi-small');
 			return imgSrc;
 		}
-		
+
 		function getResponsiAlt(el) {
 			var altValue;
 			if (el.hasAttribute('data-responsi-alt')) {
@@ -71,7 +77,7 @@ var responsi = {
 			}
 			return altValue;
 		}
-		
+
 		// Get responsi elements and loop through them, then get attributes, create the image element and append it.
 		var responsiImgs = document.querySelectorAll('.responsi-img');
 		[].forEach.call(responsiImgs, function(el) {
@@ -85,7 +91,7 @@ var responsi = {
 				el.classList.remove('responsi-img');
 			}
 		});
-		
+
 		// Responsi logic for background images.
 		var responsiBgImgs = document.querySelectorAll('.responsi-bg-img');
 		[].forEach.call(responsiBgImgs, function(el) {
@@ -94,5 +100,10 @@ var responsi = {
 			// Removing the responsi class allows us to call the function again after the first call, for example if we want to create responsi elements dynamically.
 			el.classList.remove('responsi-bg-img');
 		});
+	},
+	init : function ( responsiConfig ) {
+		// Init configs and fires the whole thing.
+		this.config( responsiConfig );
+		this.fire();
 	}
 };
